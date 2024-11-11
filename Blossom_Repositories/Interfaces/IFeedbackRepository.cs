dad0e178bcb8f528f500262e752735fbe42b3561
UBRO32BIT <90371372+UBRO32BIT@users.noreply.github.com>
2024-11-11 22:11:29 +0700

    Notification and feedback

---
+++ Blossom_Repositories/Interfaces/IFeedbackRepository.cs
@@ -0,0 +1,15 @@
+﻿using Blossom_BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blossom_Repositories.Interfaces
+{
+    public interface IFeedbackRepository
+    {
+        Task<List<Feedback>> GetFeedbackByFlowerIdAsync(string flowerId);
+        Task AddFeedbackAsync(Feedback feedback);
+    }
+}
---
+++ Blossom_Repositories/NotificationRepository.cs
@@ -0,0 +1,46 @@
+﻿using Blossom_BusinessObjects;
+using Blossom_DAOs;
+using Blossom_Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blossom_Repositories
+{
+    public class NotificationRepository : INotificationRepository
+    {
+        private readonly NotificationDAO _notificationDAO;
+
+        public NotificationRepository(NotificationDAO notificationDAO)
+        {
+            _notificationDAO = notificationDAO;
+        }
+
+        public async Task<List<Notification>> GetAllNotificationsAsync(string userId)
+        {
+            return await _notificationDAO.GetAllNotificationsAsync(userId);
+        }
+
+        public async Task<int> GetUnreadNotificationCountAsync(string userId)
+        {
+            return await _notificationDAO.GetUnreadNotificationCountAsync(userId);
+        }
+
+        public async Task AddNotificationAsync(Notification notification)
+        {
+            await _notificationDAO.AddNotificationAsync(notification);
+        }
+
+        public async Task AddMultipleNotificationsAsync(List<Notification> notifications)
+        {
+            await _notificationDAO.AddMultipleNotificationsAsync(notifications);
+        }
+
+        public async Task MarkAllNotificationsAsReadAsync(string userId)
+        {
+            await _notificationDAO.MarkAllNotificationsAsReadAsync(userId);
+        }
+    }
+}
