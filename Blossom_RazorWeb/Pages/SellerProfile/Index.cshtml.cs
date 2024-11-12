573315714000b16604d1ffda93b649efb2c67917
UBRO32BIT <90371372+UBRO32BIT@users.noreply.github.com>
2024-11-12 09:43:50 +0700

    display alert message

---
+++ Blossom_RazorWeb/Pages/SellerProfile/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blossom_RazorWeb.Pages.SellerProfile
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
