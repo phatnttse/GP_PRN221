55f4e6c493c3e83ba525627d0b7eb7bdbd74ae6b
UBRO32BIT <90371372+UBRO32BIT@users.noreply.github.com>
2024-11-12 10:17:10 +0700

    view seller profile

--- Blossom_RazorWeb/Pages/SellerProfile/Index.cshtml.cs
+++ Blossom_RazorWeb/Pages/SellerProfile/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using Blossom_BusinessObjects.Entities;
+using Blossom_Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,40 @@ namespace Blossom_RazorWeb.Pages.SellerProfile
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IFlowerService _flowerService;
+        private readonly IAccountService _accountService;
+
+        public IndexModel(IFlowerService flowerService, IAccountService accountService)
         {
+            _flowerService = flowerService;
+            _accountService = accountService;
+        }
+
+        public Account SellerInfo { get; set; }
+        public List<Flower> FlowerList { get; set; } = new List<Flower>();
+        public string SellerId { get; set; } = "defaultSellerId"; // Replace with dynamic value
+
+        public async Task<IActionResult> OnGetAsync(string sellerId)
+        {
+            if (string.IsNullOrEmpty(sellerId))
+            {
+                return RedirectToPage("/Index");
+            }
+
+            SellerId = sellerId;
+
+            if (!string.IsNullOrEmpty(SellerId))
+            {
+                SellerInfo = await _accountService.GetAccountById(SellerId);
+                FlowerList = await _flowerService.GetFlowersBySeller(SellerId);
+            }
+
+            if (SellerInfo == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
         }
     }
 }
